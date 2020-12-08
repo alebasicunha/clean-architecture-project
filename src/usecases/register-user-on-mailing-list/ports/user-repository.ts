aec0e6e27248ba758c98f41d8cac365208002874
alebasi_cunha <bela.alebasi.cunha@gmail.com>
2020-12-08 20:25:11 -0300

    feat: add user and and find user and check its existence

--- src/usecases/register-user-on-mailing-list/ports/user-repository.ts
+++ src/usecases/register-user-on-mailing-list/ports/user-repository.ts
@@ -2,7 +2,7 @@ import { UserData } from '../user-data'
 
 export interface UserRepository {
   add(user: UserData): Promise<void>
-  findUserByEmail(email: string): Promise<void>
+  findUserByEmail(email: string): Promise<UserData>
   findAllUsers(): Promise<UserData[]>
   exists(user: UserData): Promise<boolean>
 }
