4fecf3dd47c6cdbe739a3ccb5d2ccb0b6a5bb40a
alebasi_cunha <bela.alebasi.cunha@gmail.com>
2020-12-14 20:28:12 -0300

    feat: validate email larger than 320 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,9 +1,15 @@
 export class Email {
+  // estrutura do email: local-part@domain
   static validate (email : string): boolean {
+    // email nao pode ser nulo nem vazio
     if (!email) {
       return false
     }
-
+    // email não pode ser maior que 320 chars
+    if (email.length > 320) {
+      return false
+    }
+    // local part não pode ser maior que 64 char
     const [local] = email.split('@')
     if (local.length > 64) {
       return false
