373405242be7f3e855f2534d183c0f78d24dded8
alebasicunha <bela.alebasi.cunha@gmail.com>
2020-12-21 23:26:46 -0300

    test: verify name while creating user

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from '../../src/entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../src/entities/errors/invalid-name-error'
 import { User } from '../../src/entities/user'
 import { left } from '../../src/shared/either'
 
@@ -7,5 +8,17 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name (too few characters', () => {
+    const invalidname = 'O       '
+    const error = User.create({ name: invalidname, email: 'any@email.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should not create user with invalid name (too many characters', () => {
+    const invalidname = 'O'.repeat(257)
+    const error = User.create({ name: invalidname, email: 'any@email.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
