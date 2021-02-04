f5804c65619226722868fbfee8171a34f75396d5
alebasicunha <bela.alebasi.cunha@gmail.com>
2021-02-04 00:07:36 -0300

    feat: controller return 400 when user invalid/request is missing params, 500 when server raises

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -2,30 +2,23 @@ import { User, UserData } from '@/entities'
 import { Either, left, right } from '@/shared'
 import { InvalidNameError, InvalidEmailError } from '@/entities/errors'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { UseCase } from './ports/use-case'
 
-export class RegisterUserOnMailingList {
-  // Use case: create user and add to repository (FakeObject: InMemoryUserRepository)
+export class RegisterUserOnMailingList implements UseCase {
   private readonly userRepo: UserRepository
 
-  // UserRepository is a interface tha InMemoryUserRepository implements
   constructor (userRepo: UserRepository) {
     this.userRepo = userRepo
   }
 
-  // Create user with data user (request), add it to the mailing list and return user or error (with Either).
-  public async registerUserOnMailingList (request: UserData):
-    Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+  public async perform (request: UserData): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
     const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
-    // if name or email is invalid return error, user can't be created
     if (userOrError.isLeft()) {
       return left(userOrError.value)
     }
-
-    // with user created successufully, add to repository if user doesn't exsit yet
     if (!(await this.userRepo.exists(request))) {
       await this.userRepo.add(request)
     }
-
     return right(request)
   }
 }
